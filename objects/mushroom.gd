1cf2fc2459c3a6df814d76f89b75056abb65b3f1
MisterCodo <jocelyn.bergeron@gmail.com>
2022-09-28 11:27:33 -0400

    Progress on #11 Modified git config to be case sensitive, renaming folders and files

---
+++ objects/mushroom.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+var pickable = false
+
+
+func _process(delta):
+	if pickable and Input.is_action_just_pressed("action"):
+		print("picked mushroom")
+		queue_free()
+
+
+func _on_action_box_area_entered(area):
+	print("entered mushroom area")
+	pickable = true
+
+
+func _on_action_box_area_exited(area):
+	print("exited mushroom area")
+	pickable = false
