54319c80286b6ae4e0c239f93aa7e0693342bc8f
MisterCodo <jocelyn.bergeron@gmail.com>
2022-09-28 11:20:30 -0400

    Progress on #11 Renaming folders and file names for GDScript conventions

--- Characters/Slime/slime.gd
+++ Characters/Slime/slime.gd
@@ -1,26 +1,29 @@
 extends CharacterBody2D
 
+
 @export var MAX_SPEED : float = 120
 @export var ACCELERATION : float = 700
 @export var FRICTION : float = 1000
+
 
 enum SlimeClass {
 	NORMAL,
 	RADIOACTIVE
 }
+
+
 var slime_class = SlimeClass.NORMAL
-var texture_normal = preload("res://Art/SlimeSpritesheet.png")
-var texture_radioactive = preload("res://Art/SlimeSpritesheetRadioactive.png")
+var texture_normal = preload("res://assets/slime.png")
+var texture_radioactive = preload("res://assets/slime_radioactive.png")
+
 
 @onready var sprite2d_texture = $Sprite2d
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
-func _ready():
-	pass
-	
+
 func _physics_process(delta):
-	# look for change slime class action
+	# Look for change slime class action
 	if Input.is_action_just_pressed("change_slime_class"):
 		match slime_class:
 			SlimeClass.NORMAL:
@@ -30,15 +33,15 @@ func _physics_process(delta):
 				slime_class = SlimeClass.NORMAL
 				sprite2d_texture.set_texture(texture_normal)
 		
-	# move slime based on input direction
+	# Move slime based on input direction
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if input_direction == Vector2.ZERO:
-		# player is not pressing any moving key
+		# Player is not pressing any moving key
 		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	else:
-		# player is pressing keys to move
+		# Player is pressing keys to move
 		animation_state.travel("Walk")
 		animation_tree.set("parameters/Idle/blend_position", input_direction)
 		animation_tree.set("parameters/Walk/blend_position", input_direction)
@@ -46,4 +49,3 @@ func _physics_process(delta):
 		
 	set_velocity(velocity)
 	move_and_slide()
-	
