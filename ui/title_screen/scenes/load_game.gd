b51c7a4d46c96b4b09f891a2ac850e68661b134a
MisterCodo <jocelyn.bergeron@gmail.com>
2022-11-01 17:04:02 -0400

    Changed look of load_game scene, grab focus on top save file

--- ui/title_screen/scenes/load_game.gd
+++ ui/title_screen/scenes/load_game.gd
@@ -5,12 +5,16 @@ func _ready():
 	var save_files = Global.list_save_files()
 	var placeholder = $CenterContainer/VBoxContainer/VBoxContainer
 	var button_pck = preload("res://ui/title_screen/game_overview_button.tscn")
+	var first_button = true
 	for save_file in save_files:
 		var button = button_pck.instantiate()
 		button.game_name = save_file.game_name
 		button.datetime = save_file.save_datetime
 		button.save_file = save_file.save_file
 		placeholder.add_child(button)
+		if first_button:
+			button.grab_focus()
+		first_button = false
 
 
 func _on_back_pressed():
